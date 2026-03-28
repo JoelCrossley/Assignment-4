6f181e827141dc501e05469003bcf4e577158dfe
JoelCrossley <joelcrossley7@gmail.com>
2026-03-28 18:40:58 +0000

    Update main.cpp
    Adds input and validation for particles and momenta, outputs results of dot product and vector sum of four momenta

--- main.cpp
+++ main.cpp
@@ -7,48 +7,35 @@
 #include<vector>
 
 using namespace std;
-/*
-class fourMomentum
-{
-    double energy = 0.0;
-    double xMomentum = 0.0;
-    double yMomentum = 0.0;
-    double zMomentum = 0.0;
-};
-*/
 
 class particle
 {
 private:
     std::string type {"Ghost"};
     double mass {0.0};
-    std::vector<double> fourMomentum {0.0, 0.0, 0.0, 0.0};
+    std::vector<double> fourMomentum {0.0, 0.0, 0.0, 0.0};//introduce particle with default values
 public:
     // Default constructor
     particle() = default ;
 
     double ThreeMagnitude(vector<double> threeVector) {
-        return pow( pow(threeVector[1],2) + pow(threeVector[2],2) + pow(threeVector[3],2) , 0.5);
+        return pow( pow(threeVector[1],2) + pow(threeVector[2],2) + pow(threeVector[3],2) , 0.5);//function to calculate the magnitude of a particle's momentum
     }
 
     // Parameterized constructor
-    particle(string particleType, double xMomentum, double yMomentum, double zMomentum) {
+    particle(string particleType, double xMomentum, double yMomentum, double zMomentum) {//particle type and the three momentum components is enough to assign all the properties of the particles, since the mass determined by the particle type and energy from mass and momentum
         type = particleType;
         fourMomentum[1] = xMomentum;
         fourMomentum[2] = yMomentum;
         fourMomentum[3] = zMomentum;
         if (type == "electron") mass = 0.511;
         else if (type == "muon") mass = 105.6;
-        else if (type == "tau") mass = 1776.9;
+        else if (type == "tau") mass = 1776.9;//mass assigned to particles based on their type
 
-        fourMomentum[0] = pow(mass*mass + pow(ThreeMagnitude(fourMomentum),2) ,0.5);
+        fourMomentum[0] = pow(mass*mass + pow(ThreeMagnitude(fourMomentum),2) ,0.5);//energy calculated from total momentum and mass
     }
 
-    ~particle(){std::cout<<"Destroying "<<type<<std::endl;}  // Destructor (in-line)
-    double gamma() {return fourMomentum[0]/mass;} // One-line functions are OK in-line
-    void print_data();
-
-    double getFourEnergy() {
+    double getFourEnergy() {//getters for each component of four momentum
         return fourMomentum[0];
     }
     double getFourMomentumX() {
@@ -61,18 +48,23 @@ public:
         return fourMomentum[3];
     }
 
+    ~particle(){} //{std::cout<<"Destroying "<<type<<std::endl;}  // Destructor (in-line)
+    double gamma() {return fourMomentum[0]/mass;} // One-line functions are OK in-line
+    void print_data();
+
 };
 #endif
 
-vector<double> DotProduct(particle p1, particle p2) {
+vector<double> DotProduct(particle p1, particle p2) {//function to take the dot product of two momenta
+    double energy = p1.getFourEnergy() * p2.getFourEnergy();
     double xMomentum = p1.getFourMomentumX()*p2.getFourMomentumX();
     double yMomentum = p1.getFourMomentumY()*p2.getFourMomentumY();
     double zMomentum = p1.getFourMomentumZ()*p2.getFourMomentumZ();
-    vector<double> dotMomentum = {xMomentum,yMomentum,zMomentum};
+    vector<double> dotMomentum = {energy, xMomentum,yMomentum,zMomentum};
     return dotMomentum;
 }
 
-vector<double> VectorSum(particle p1, particle p2) {
+vector<double> VectorSum(particle p1, particle p2) {//adds to four momenta
     double energy = p1.getFourEnergy() + p2.getFourEnergy();
     double xMomentum = p1.getFourMomentumX()+p2.getFourMomentumX();
     double yMomentum = p1.getFourMomentumY()+p2.getFourMomentumY();
@@ -81,13 +73,89 @@ vector<double> VectorSum(particle p1, particle p2) {
     return MomentumSum;
 }
 
-double ThreeMagnitude(vector<double> threeVector) {
+double ThreeMagnitude(vector<double> threeVector) {//function to calculate the magnitude of a particle's momentum
     return pow( pow(threeVector[0],2) + pow(threeVector[1],2) + pow(threeVector[2],2) , 0.5);
 }
 
+int NumValidation(int type) //Function used to validate integer inputs
+{
+    while (!(cin>>type)|| type<1|| type > 3) {
+        cout << "Invalid input, must be a 1, 2 or 3 " << endl;
+        //if (type>nInitial){cout << "that is less than the initial energy level" ;}
+        cin.clear();
+        cin.ignore();
+    }
+    return type;
+}
+
+int MomentumValidation(int momentum) //Function used to validate momentum inputs
+{
+    while (!(cin>>momentum)) {
+        cout << "Invalid input, must be a double " << endl;
+        //if (type>nInitial){cout << "that is less than the initial energy level" ;}
+        cin.clear();
+        cin.ignore();
+    }
+    return momentum;
+}
+
 int main() {
-    particle particle1("tau", 100, 0, 0);
-    particle particle2("muon", 100, 0, 0);
-    cout << particle1.getFourMomentumX() << endl;
-    cout << DotProduct(particle1, particle2)[0] << endl;
+    //particle particle1;
+    //particle particle2;
+    int typeNum;
+    string particleType;
+    double xMomentum, yMomentum, zMomentum;
+    cout << "Choose type for the first particle, 1 for electron, 2 for muon or 3 for tau" << endl;
+    typeNum = NumValidation(typeNum);
+    cout << "Input particle 1 x momentum"<<endl;
+    xMomentum = MomentumValidation(xMomentum);
+    cout << "Input particle 1 y momentum"<<endl;
+    yMomentum = MomentumValidation(yMomentum);
+    cout << "Input particle 1 z momentum"<<endl;
+    zMomentum = MomentumValidation(zMomentum);
+
+    if (typeNum == 1) {
+        particleType = "electron";
+        cout << "Electron selected"<<endl;
+    } else if (typeNum == 2) {
+        particleType = "muon";
+        cout << "Muon selected"<<endl;
+    } else if (typeNum == 3) {
+        particleType = "tau";
+        cout << "Tau selected"<<endl;
+    }
+
+    particle particle1(particleType, xMomentum, yMomentum, zMomentum);
+
+    cout << "Choose type for the second particle, 1 for electron, 2 for muon or 3 for tau" << endl;
+    typeNum = NumValidation(typeNum);
+    cout << "Input particle 2 x momentum"<<endl;
+    xMomentum = MomentumValidation(xMomentum);
+    cout << "Input particle 2 y momentum"<<endl;
+    yMomentum = MomentumValidation(yMomentum);
+    cout << "Input particle 2 z momentum"<<endl;
+    zMomentum = MomentumValidation(zMomentum);
+
+    if (typeNum == 1) {
+        particleType = "electron";
+        cout << "Electron selected"<<endl;
+    } else if (typeNum == 2) {
+        particleType = "muon";
+        cout << "Muon selected"<<endl;
+    } else if (typeNum == 3) {
+        particleType = "tau";
+        cout << "Tau selected"<<endl;
+    }
+
+    particle particle2(particleType, xMomentum, yMomentum, zMomentum);
+
+    cout << "The sum of the four momenta is (";
+    for (int i = 0; i < 4; i++) {
+        cout << VectorSum(particle1, particle2)[i] << " , ";
+    }
+    cout << " )" << endl << "The dot product of the four momenta is (";
+    for (int i = 0; i < 4; i++) {
+        cout << DotProduct(particle1, particle2)[i] << " , ";
+    }
+    cout << ")";
 }
